13c7f9d8b6bb0243543a2bb44132da2fc55b785d
Elif Bihter KUŞÇU <56038735+bihterkuscu@users.noreply.github.com>
2022-07-25 09:15:52 +0300

    TASK IOS-28:  Section 4: Create the Dicee app
    Dicee app create

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    //IBOutlet allows me to the reference  a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    /*var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //WHO          WHAT    VALUE
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+         
+    } */
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        /*
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+        //write debug console
+        //print("Button got tapped.")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+         */
     }
-
-
+    
+    
 }
 
